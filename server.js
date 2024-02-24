cf36a6ef6b7ce4af0849e09165adeeeade6cea32
sebalp100 <sebalp100@gmail.com>
2024-02-23 23:13:47 -0300

    Add base files

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const studentRoutes = require("./src/student/routes")
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+})
+
+app.use("/api/v1/students", studentRoutes);
+
+app.listen(port, () => console.log(`app listening on port ${port}`));
