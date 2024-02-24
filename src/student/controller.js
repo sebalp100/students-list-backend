7d1de5e9b01f1f37f45e9bd0d42a71f67a17227a
sebalp100 <sebalp100@gmail.com>
2024-02-24 00:18:28 -0300

    Add update endpoint

--- src/student/controller.js
+++ src/student/controller.js
@@ -27,7 +27,7 @@ const addStudent = (req, res) => {
 
       pool.query(queries.addStudent, [name, email, age, dob], (error, results) => {
         if (error) throw error;
-        res.status(201).send("Student created successfully");
+        res.status(201).send("Student created successfully.");
       })
     }
   });
@@ -40,19 +40,44 @@ const removeStudent = (req, res) => {
   pool.query(queries.getStudentById, [id], (error, results) => {
     const noStudentFound = !results.rows.length;
     if (noStudentFound) {
-      res.send("Student does not exists in our database");
+      res.send("Student does not exists in our database.");
     }
 
     pool.query(queries.removeStudent, [id], (error, results) => {
       if (error) throw error;
-      res.status(200).send("Student deleted successfully");
+      res.status(200).send("Student deleted successfully.");
     })
   })
 }
+
+const updateStudent = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name } = req.body;
+
+  if (!name || Object.keys(req.body).length !== 1) {
+    return res.status(400).send("Only the name field can be updated.");
+  }
+
+  pool.query(queries.getStudentById, [id], (error, results) => {
+    const noStudentFound = !results.rows.length;
+    if (noStudentFound) {
+      return res.status(404).send("Student does not exist in our database.");
+    } else {
+      pool.query(queries.updateStudent, [name, id], (error, results) => {
+        if (error) {
+          console.error("Error updating student:", error);
+          return res.status(500).send("Internal Server Error");
+        }
+        res.status(200).send("Student name updated successfully.");
+      });
+    }
+  });
+};
 
 module.exports = {
   getStudents,
   getStudentById,
   addStudent,
   removeStudent,
+  updateStudent,
 }
